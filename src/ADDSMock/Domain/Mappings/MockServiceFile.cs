179a7715cb5035ccfe8ae3bfb0ee412b91dffc18
John Rippington <john@portolan.co>
2025-02-27 08:39:01 +0000

    Corrected name and path for MockServiceFile

--- src/ADDSMock/Domain/Mappings/MockServiceFile.cs
+++ src/ADDSMock/Domain/Mappings/MockServiceFile.cs
@@ -7,7 +7,7 @@
         private readonly string _mimeType;
         private readonly bool _isOverride;
 
-        public MockServiceFile(string path, string name, string mimeType, bool isOverride)
+        public MockServiceFile(string name, string path, string mimeType, bool isOverride)
         {
             _name = name;
             _path = path;
