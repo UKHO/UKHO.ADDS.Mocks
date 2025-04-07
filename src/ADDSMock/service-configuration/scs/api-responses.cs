31820f07956c44bf0c74c22eeeb1d3496a66a513
Arit Sarkar <arit14975@mastek.com>
2025-04-07 19:02:41 +0530

    212192 - Updated Catlogue.json and scs response file AB#212192 AB#213340

--- src/ADDSMock/service-configuration/scs/api-responses.cs
+++ src/ADDSMock/service-configuration/scs/api-responses.cs
@@ -22,20 +22,20 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
     server
         .Given(
             Request.Create()
-                .WithParam("If-Modified-Since", "2020-10-27T00:00:00Z")
+                .WithUrl(new RegexMatcher(".*/v2/catalogues/s100/basic.*"))
+                .WithHeader("If-Modified-Since", "2025-01-01T00:00:00Z")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(304)
-                .WithHeader("Content-Type", "application/json")
-                .WithBody("Not modified.")
         );
 
     server
         .Given(
             Request.Create()
-                .WithParam("If-Modified-Since", "20221027")
+                .WithUrl(new RegexMatcher(".*/v2/catalogues/s100/basic.*"))
+                .WithHeader("If-Modified-Since", "20221027")
                 .UsingGet()
         )
         .RespondWith(
@@ -48,7 +48,8 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
     server
         .Given(
             Request.Create()
-                .WithParam("If-Modified-Since", "3000-01-01T00:00:00Z")
+                .WithUrl(new RegexMatcher(".*/v2/catalogues/s100/basic.*"))
+                .WithHeader("If-Modified-Since", "3000-01-01T00:00:00Z")
                 .UsingGet()
         )
         .RespondWith(
