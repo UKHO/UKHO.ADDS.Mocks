534176c39fcfabe952c0025223d50508d405b037
Pradeep I <pradeep15558@mastek.com>
2025-04-16 17:10:32 +0530

    AB# 214778 - Added additional error codes for FSS Mock - batch creation end point.  (QA)

--- src/ADDSMock/service-configuration/fss/api-responses.cs
+++ src/ADDSMock/service-configuration/fss/api-responses.cs
@@ -53,4 +53,65 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
              .WithHeader("Content-Type", "application/json")
              .WithBody("Invalid Expiry Date Format.")
      );
+
+    server
+        .Given(
+            Request.Create()
+                .WithPath(urlPattern)
+                .UsingPost()
+                .WithBody(new RegexMatcher(@".*"))
+                .WithHeader("_X-Correlation-ID", "400-badrequest-guid-fss")
+        )
+        .RespondWith(
+            Response.Create()
+                .WithStatusCode(400)
+                .WithHeader("Content-Type", "application/json")
+                .WithBody("Bad request.")
+        );
+
+    server
+        .Given(
+            Request.Create()
+                .WithPath(urlPattern)
+                .UsingPost()
+                .WithBody(new RegexMatcher(@".*"))
+                .WithHeader("_X-Correlation-ID", "401-unauthorised-guid-fss")
+        )
+        .RespondWith(
+            Response.Create()
+                .WithStatusCode(401)
+                .WithHeader("Content-Type", "application/json")
+                .WithBody("Unauthorised.")
+        );
+
+    server
+        .Given(
+            Request.Create()
+                .WithPath(urlPattern)
+                .UsingPost()
+                .WithBody(new RegexMatcher(@".*"))
+                .WithHeader("_X-Correlation-ID", "403-forbidden-guid-fss")
+        )
+        .RespondWith(
+            Response.Create()
+                .WithStatusCode(403)
+                .WithHeader("Content-Type", "application/json")
+                .WithBody("Forbidden.")
+        );
+
+    server
+        .Given(
+            Request.Create()
+                .WithPath(urlPattern)
+                .UsingPost()
+                .WithBody(new RegexMatcher(@".*"))
+                .WithHeader("_X-Correlation-ID", "429-toomanyrequests-guid-fss")
+        )
+        .RespondWith(
+            Response.Create()
+                .WithStatusCode(429)
+                .WithHeader("Content-Type", "application/json")
+                .WithHeader("Retry-After", "10")
+                .WithBody("Too Many Requests.")
+        );
 }
