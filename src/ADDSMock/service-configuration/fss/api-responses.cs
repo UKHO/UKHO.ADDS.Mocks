445a58a748c912c80ca47e25194649b169a1bfb7
rushike15306 <108336693+rushike15306@users.noreply.github.com>
2025-04-16 17:58:08 +0530

    Code Changes AB#215460

--- src/ADDSMock/service-configuration/fss/api-responses.cs
+++ src/ADDSMock/service-configuration/fss/api-responses.cs
@@ -4,35 +4,34 @@ using WireMock.Matchers;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 
-
 public void RegisterFragment(WireMockServer server, MockService mockService)
 {
-    var urlPattern = "/batch/{batchId}/files/{fileName}";
+    var urlPattern = ".*/batch/{batchId}/files/{fileName}.*";
 
     server
         .Given(
             Request.Create()
                 .WithPath(new RegexMatcher(urlPattern))
                 .UsingGet()
-                .WithPath(new RegexMatcher(@"/batch/[a-zA-Z0-9-]+/files/[a-zA-Z0-9]+\.[0-9]{3}$")) // Filter for valid file paths
+                .WithPath(new RegexMatcher(@"/batch/[a-fA-F0-9-]{36}/files/"))
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(200)
                 .WithHeader("Content-Type", "application/octet-stream")
+                .WithBodyFromFile(mockService.Files.Where(x => x.Name == "MockFile.txt").Select(x => x.Path).FirstOrDefault())
         );
 
     server
         .Given(
             Request.Create()
-                .WithPath(urlPattern)
+                .WithPath(new RegexMatcher(@"^(?!/fss/batch/[a-fA-F0-9-]{36}/files/).*"))
                 .UsingGet()
-                .WithPath(new RegexMatcher(@"^(?!/fss/batch/s100-batch/files/[a-zA-Z0-9]+\.[0-9]{3}$).*")) // Filter for invalid file
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(400)
                 .WithHeader("Content-Type", "application/json")
-                .WithBody("invalid batch id or filename.")
+                .WithBody("{\"error\": \"Invalid batchId.\"}")
         );
 }
