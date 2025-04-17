15327b31566c0dce646a004ea46811f82a455864
Sheetal Dalavi <sheetal15692@mastek.com>
2025-04-17 17:37:56 +0530

    AB#215435 Added Mock error codes

--- src/ADDSMock/service-configuration/fss/AddFile.cs
+++ src/ADDSMock/service-configuration/fss/AddFile.cs
@@ -7,13 +7,13 @@ using WireMock.ResponseBuilders;
 public void RegisterFragment(WireMockServer server, MockService mockService)
 {
     var urlPattern = "/batch/(.*)/files/(.*)";
-    
+
     server
         .Given(
             Request.Create()
                 .WithPath(urlPattern)
                 .UsingPost()
-                .WithHeader("X-Content-Size", new RegexMatcher(".*")) 
+                .WithHeader("X-Content-Size", new RegexMatcher(".*"))
                 .WithBody(new JsonMatcher(@"
            {
             ""attributes"": [
@@ -28,17 +28,61 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithStatusCode(201)
                 .WithHeader("Content-Type", "application/json")
         );
-    
+
+    server
+         .Given(
+             Request.Create()
+                 .WithPath(new RegexMatcher(urlPattern))
+                 .WithHeader("_X-Correlation-ID", "400-badrequest-fss-add-file")
+                 .UsingPost()
+         )
+         .RespondWith(
+             Response.Create()
+                 .WithStatusCode(400)
+                 .WithHeader("Content-Type", "application/json")
+                 .WithBody("Bad Request")
+         );
+
     server
         .Given(
             Request.Create()
-                .WithPath(new RegexMatcher("^/batch//files/.*$")) // Matches if batchId is missing
+                .WithPath(new RegexMatcher(urlPattern))
+                .WithHeader("_X-Correlation-ID", "401-unauthorized-add-file")
                 .UsingPost()
         )
         .RespondWith(
             Response.Create()
-                .WithStatusCode(400) // Bad Request
+                .WithStatusCode(401)
                 .WithHeader("Content-Type", "application/json")
-                .WithBody(@"{ ""error"": ""Batch ID is missing in the URI."" }")
+                .WithBody("Unauthorized")
         );
+
+    server
+         .Given(
+             Request.Create()
+                 .WithPath(new RegexMatcher(urlPattern))
+                 .WithHeader("_X-Correlation-ID", "403-forbidden-fss-add-file")
+                 .UsingPost()
+         )
+         .RespondWith(
+             Response.Create()
+                 .WithStatusCode(403)
+                 .WithHeader("Content-Type", "application/json")
+                 .WithBody("Forbidden")
+         );
+
+    server
+         .Given(
+             Request.Create()
+                 .WithPath(urlPattern)
+                 .UsingPost()
+                 .WithHeader("_X-Correlation-ID", "429-toomanyrequests-guid-fss-add-file")
+         )
+         .RespondWith(
+             Response.Create()
+                 .WithStatusCode(429)
+                 .WithHeader("Content-Type", "application/json")
+                 .WithHeader("Retry-After", "10")
+                 .WithBody("Too Many Requests")
+         );
 }
