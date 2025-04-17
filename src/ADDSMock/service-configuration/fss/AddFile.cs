b2b2024ded03fb4d47ebf1b8614ccac593d59786
Ganpat Gawde <Ganpat14802@Mastek.Com>
2025-04-17 12:00:23 +0530

    added coading changes for FSS Mock addfile endpoint.(status code 201 & 400)

---
+++ src/ADDSMock/service-configuration/fss/AddFile.cs
@@ -0,0 +1,44 @@
+﻿using ADDSMock.Domain.Mappings;
+using System.Net;
+using WireMock.Matchers;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+public void RegisterFragment(WireMockServer server, MockService mockService)
+{
+    var urlPattern = "/batch/(.*)/files/(.*)";
+    
+    server
+        .Given(
+            Request.Create()
+                .WithPath(urlPattern)
+                .UsingPost()
+                .WithHeader("X-Content-Size", new RegexMatcher(".*")) 
+                .WithBody(new JsonMatcher(@"
+           {
+            ""attributes"": [
+              {
+                ""key"": ""string"",
+                ""value"": ""string""
+              }]
+           }"))
+        )
+        .RespondWith(
+            Response.Create()
+                .WithStatusCode(201)
+                .WithHeader("Content-Type", "application/json")
+        );
+    
+    server
+        .Given(
+            Request.Create()
+                .WithPath(new RegexMatcher("^/batch//files/.*$")) // Matches if batchId is missing
+                .UsingPost()
+        )
+        .RespondWith(
+            Response.Create()
+                .WithStatusCode(400) // Bad Request
+                .WithHeader("Content-Type", "application/json")
+                .WithBody(@"{ ""error"": ""Batch ID is missing in the URI."" }")
+        );
+}
