23a46cb25500547feb46cc4832e1f3dbaf0cffc4
Pradeep I <pradeep15558@mastek.com>
2025-04-21 13:19:03 +0530

    AB# 215959 - Appended end point name in correlation id

--- src/ADDSMock/service-configuration/scs/basic-catalogue.cs
+++ src/ADDSMock/service-configuration/scs/basic-catalogue.cs
@@ -19,7 +19,7 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithStatusCode(200)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("Last-Modified", DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"))
-                .WithHeader("_X-Correlation-ID", "200-ok-guid-scs")
+                .WithHeader("_X-Correlation-ID", "200-ok-guid-scs-basic-catalogue")
                 .WithBodyFromFile(mockService.Files.FirstOrDefault()?.Path)
         );
 
@@ -27,28 +27,28 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
     .Given(
         Request.Create()
             .WithUrl(new RegexMatcher(urlPattern))
-            .WithHeader("_X-Correlation-ID", "304-notmodified-guid-scs")
+            .WithHeader("_X-Correlation-ID", "304-notmodified-guid-scs-basic-catalogue")
             .UsingGet()
     )
     .RespondWith(
         Response.Create()
             .WithStatusCode(304)
             .WithHeader("Last-Modified", DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"))
-            .WithHeader("_X-Correlation-ID", "304-notmodified-guid-scs")
+            .WithHeader("_X-Correlation-ID", "304-notmodified-guid-scs-basic-catalogue")
     );
 
     server
         .Given(
             Request.Create()
                 .WithUrl(new RegexMatcher(urlPattern))
-                .WithHeader("_X-Correlation-ID", "400-badrequest-guid-scs")
+                .WithHeader("_X-Correlation-ID", "400-badrequest-guid-scs-basic-catalogue")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(400)
                 .WithHeader("Content-Type", "application/json")
-                .WithHeader("_X-Correlation-ID", "400-badrequest-guid-scs")
+                .WithHeader("_X-Correlation-ID", "400-badrequest-guid-scs-basic-catalogue")
                 .WithBody("Bad request.")
         );
 
@@ -56,14 +56,14 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
         .Given(
             Request.Create()
                 .WithUrl(new RegexMatcher(urlPattern))
-                .WithHeader("_X-Correlation-ID", "500-internalserver-guid-scs")
+                .WithHeader("_X-Correlation-ID", "500-internalserver-guid-scs-basic-catalogue")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(500)
                 .WithHeader("Content-Type", "application/json")
-                .WithHeader("_X-Correlation-ID", "500-internalserver-guid-scs")
+                .WithHeader("_X-Correlation-ID", "500-internalserver-guid-scs-basic-catalogue")
                 .WithBody("Internal server error.")
         );
 
@@ -71,14 +71,14 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
         .Given(
             Request.Create()
                 .WithUrl(new RegexMatcher(urlPattern))
-                .WithHeader("_X-Correlation-ID", "401-unauthorised-guid-scs")
+                .WithHeader("_X-Correlation-ID", "401-unauthorised-guid-scs-basic-catalogue")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(401)
                 .WithHeader("Content-Type", "application/json")
-                .WithHeader("_X-Correlation-ID", "401-unauthorised-guid-scs")
+                .WithHeader("_X-Correlation-ID", "401-unauthorised-guid-scs-basic-catalogue")
                 .WithBody("Unauthorised.")
         );
 
@@ -86,14 +86,14 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
         .Given(
             Request.Create()
                 .WithUrl(new RegexMatcher(urlPattern))
-                .WithHeader("_X-Correlation-ID", "403-forbidden-guid-scs")
+                .WithHeader("_X-Correlation-ID", "403-forbidden-guid-scs-basic-catalogue")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(403)
                 .WithHeader("Content-Type", "application/json")
-                .WithHeader("_X-Correlation-ID", "403-forbidden-guid-scs")
+                .WithHeader("_X-Correlation-ID", "403-forbidden-guid-scs-basic-catalogue")
                 .WithBody("Forbidden.")
         );
 
@@ -101,14 +101,14 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
         .Given(
             Request.Create()
                 .WithUrl(new RegexMatcher(urlPattern))
-                .WithHeader("_X-Correlation-ID", "404-notfound-guid-scs")
+                .WithHeader("_X-Correlation-ID", "404-notfound-guid-scs-basic-catalogue")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(404)
                 .WithHeader("Content-Type", "application/json")
-                .WithHeader("_X-Correlation-ID", "404-notfound-guid-scs")
+                .WithHeader("_X-Correlation-ID", "404-notfound-guid-scs-basic-catalogue")
                 .WithBody("Not found.")
         );
 
@@ -116,14 +116,14 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
        .Given(
            Request.Create()
                .WithUrl(new RegexMatcher(urlPattern))
-               .WithHeader("_X-Correlation-ID", "415-unsupportedmediatype-guid-scs")
+               .WithHeader("_X-Correlation-ID", "415-unsupportedmediatype-guid-scs-basic-catalogue")
                .UsingGet()
        )
        .RespondWith(
            Response.Create()
                .WithStatusCode(415)
                .WithHeader("Content-Type", "application/json")
-               .WithHeader("_X-Correlation-ID", "415-unsupportedmediatype-guid-scs")
+               .WithHeader("_X-Correlation-ID", "415-unsupportedmediatype-guid-scs-basic-catalogue")
                .WithBody("Unsupported Media Type.")
        );
 }
