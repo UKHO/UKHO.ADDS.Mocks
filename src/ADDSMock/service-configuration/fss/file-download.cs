9578c1a1b130a5336f89b585144f2c779772ac50
rushike15306 <108336693+rushike15306@users.noreply.github.com>
2025-04-22 17:19:25 +0530

    remove Body from error code AB#215460

--- src/ADDSMock/service-configuration/fss/file-download.cs
+++ src/ADDSMock/service-configuration/fss/file-download.cs
@@ -18,7 +18,6 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
     server
         .Given(
             Request.Create()
-                .WithPath(new RegexMatcher(urlPattern))
                 .WithPath(new RegexMatcher(@"/batch/[a-fA-F0-9-]{36}/files/"))
                 .UsingGet()
         )
@@ -92,7 +91,6 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithStatusCode(401)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("_X-Correlation-ID", "401-unauthorized-guid-fss-file-download")
-                .WithBody("Unauthorized")
         );
 
     // 403 Forbidden Response
@@ -108,7 +106,6 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithStatusCode(403)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("_X-Correlation-ID", "403-forbidden-guid-fss-file-download")
-                .WithBody("Forbidden")
         );
 
     // 404 File Not Found Response
@@ -124,7 +121,6 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithStatusCode(404)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("_X-Correlation-ID", "404-filenotfound-guid-fss-file-download")
-                .WithBody("File Not Found")
         );
 
     // 410 Gone Response
@@ -140,7 +136,6 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithStatusCode(410)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("_X-Correlation-ID", "410-gone-guid-fss-file-download")
-                .WithBody("Gone")
         );
 
     // 416 Range Not Satisfiable Response
@@ -148,15 +143,14 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
         .Given(
             Request.Create()
                 .WithPath(new RegexMatcher(urlPattern))
-                .WithHeader("Range", "416-rangenotsatifiable-guid-fss-file-download")
+                .WithHeader("_X-Correlation-ID", "416-rangenotsatifiable-guid-fss-file-download")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(416)
                 .WithHeader("Content-Type", "application/json")
-                .WithHeader("Range", "416-rangenotsatifiable-guid-fss-file-download")
-                .WithBody("Range Not Satisfiable")
+                .WithHeader("_X-Correlation-ID", "416-rangenotsatifiable-guid-fss-file-download")
         );
 
     // 429 Too Many Requests Response
@@ -173,7 +167,6 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("Retry-After", "10")
                 .WithHeader("_X-Correlation-ID", "429-toomanyrequests-guid-fss-file-download")
-                .WithBody("Too Many Requests")
         );
 
     // 307 Temporary Redirect Response
@@ -190,6 +183,5 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("Redirect-Location", "https://example.com/redirect")
                 .WithHeader("_X-Correlation-ID", "307-temporaryredirect-guid-fss-file-download")
-                .WithBody("Temporary Redirect")
         );
 }
