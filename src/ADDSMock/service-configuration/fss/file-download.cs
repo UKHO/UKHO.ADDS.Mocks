da438880d18b97f57dcb543548a77a134c236f3d
Ganpat Gawde <Ganpat14802@Mastek.Com>
2025-04-22 13:43:09 +0530

    added 400 response status message.

--- src/ADDSMock/service-configuration/fss/file-download.cs
+++ src/ADDSMock/service-configuration/fss/file-download.cs
@@ -14,6 +14,7 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
 {
     var urlPattern = ".*/batch/(.*)/files/(.*)";
 
+    // 200 OK Response with File Download
     server
         .Given(
             Request.Create()
@@ -27,7 +28,12 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 {
                     var pathSegments = request.PathSegments;
                     var fileName = pathSegments.ElementAtOrDefault(4);
-                    var fileResponse = System.IO.File.ReadAllBytes(mockService.Files.Where(x => x.Name == "mock-file.txt").Select(x => x.Path).FirstOrDefault());
+                    var fileResponse = System.IO.File.ReadAllBytes(
+                        mockService.Files
+                            .Where(x => x.Name == "mock-file.txt")
+                            .Select(x => x.Path)
+                            .FirstOrDefault()
+                    );
 
                     return new WireMock.ResponseMessage
                     {
@@ -46,21 +52,34 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 })
         );
 
+    // 400 Bad Request Response
     server
         .Given(
             Request.Create()
                 .WithPath(new RegexMatcher(@"/batch/[a-fA-F0-9-]{36}/files/"))
-                .WithHeader("_X-Correlation-ID", "400-invalidbatchid-guid-fss-file-download")
+                .WithHeader("_X-Correlation-ID", "400-badrequest-guid-fss-file-download")
                 .UsingGet()
         )
         .RespondWith(
             Response.Create()
                 .WithStatusCode(400)
                 .WithHeader("Content-Type", "application/json")
-                .WithHeader("_X-Correlation-ID", "400-invalidbatchid-guid-fss-file-download")
-                .WithBody("{\"error\": \"Invalid batchId.\"}")
+                .WithHeader("_X-Correlation-ID", "400-badrequest-guid-fss-file-download")
+                .WithBodyAsJson(new
+                {
+                    correlationId = "400-badrequest-guid-fss-file-download",
+                    errors = new[]
+                    {
+                        new
+                        {
+                            source = "File Download",
+                            description = "Invalid batchId."
+                        }
+                    }
+                })
         );
 
+    // 401 Unauthorized Response
     server
         .Given(
             Request.Create()
@@ -76,75 +95,80 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithBody("Unauthorized")
         );
 
+    // 403 Forbidden Response
     server
         .Given(
-             Request.Create()
+            Request.Create()
                 .WithPath(new RegexMatcher(urlPattern))
                 .WithHeader("_X-Correlation-ID", "403-forbidden-guid-fss-file-download")
                 .UsingGet()
         )
         .RespondWith(
-             Response.Create()
+            Response.Create()
                 .WithStatusCode(403)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("_X-Correlation-ID", "403-forbidden-guid-fss-file-download")
                 .WithBody("Forbidden")
         );
 
+    // 404 File Not Found Response
     server
         .Given(
-             Request.Create()
+            Request.Create()
                 .WithPath(new RegexMatcher(urlPattern))
                 .UsingGet()
                 .WithHeader("_X-Correlation-ID", "404-filenotfound-guid-fss-file-download")
         )
         .RespondWith(
-              Response.Create()
+            Response.Create()
                 .WithStatusCode(404)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("_X-Correlation-ID", "404-filenotfound-guid-fss-file-download")
                 .WithBody("File Not Found")
         );
 
+    // 410 Gone Response
     server
         .Given(
-             Request.Create()
+            Request.Create()
                 .WithPath(new RegexMatcher(urlPattern))
                 .UsingGet()
                 .WithHeader("_X-Correlation-ID", "410-gone-guid-fss-file-download")
         )
         .RespondWith(
-             Response.Create()
+            Response.Create()
                 .WithStatusCode(410)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("_X-Correlation-ID", "410-gone-guid-fss-file-download")
                 .WithBody("Gone")
         );
 
+    // 416 Range Not Satisfiable Response
     server
         .Given(
-             Request.Create()
+            Request.Create()
                 .WithPath(new RegexMatcher(urlPattern))
                 .WithHeader("Range", "416-rangenotsatifiable-guid-fss-file-download")
                 .UsingGet()
         )
         .RespondWith(
-             Response.Create()
+            Response.Create()
                 .WithStatusCode(416)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("Range", "416-rangenotsatifiable-guid-fss-file-download")
                 .WithBody("Range Not Satisfiable")
         );
 
+    // 429 Too Many Requests Response
     server
         .Given(
-             Request.Create()
+            Request.Create()
                 .WithPath(new RegexMatcher(urlPattern))
                 .UsingGet()
                 .WithHeader("_X-Correlation-ID", "429-toomanyrequests-guid-fss-file-download")
         )
         .RespondWith(
-             Response.Create()
+            Response.Create()
                 .WithStatusCode(429)
                 .WithHeader("Content-Type", "application/json")
                 .WithHeader("Retry-After", "10")
@@ -152,19 +176,20 @@ public void RegisterFragment(WireMockServer server, MockService mockService)
                 .WithBody("Too Many Requests")
         );
 
+    // 307 Temporary Redirect Response
     server
-          .Given(
-              Request.Create()
-                 .WithPath(new RegexMatcher(urlPattern))
-                 .UsingGet()
-                 .WithHeader("_X-Correlation-ID", "307-temporaryredirect-guid-fss-file-download")
-          )
-          .RespondWith(
-              Response.Create()
-                 .WithStatusCode(307)
-                 .WithHeader("Content-Type", "application/json")
-                 .WithHeader("Redirect-Location", "https://example.com/redirect")
-                 .WithHeader("_X-Correlation-ID", "307-temporaryredirect-guid-fss-file-download")
-                 .WithBody("Temporary Redirect")
-          );
+        .Given(
+            Request.Create()
+                .WithPath(new RegexMatcher(urlPattern))
+                .UsingGet()
+                .WithHeader("_X-Correlation-ID", "307-temporaryredirect-guid-fss-file-download")
+        )
+        .RespondWith(
+            Response.Create()
+                .WithStatusCode(307)
+                .WithHeader("Content-Type", "application/json")
+                .WithHeader("Redirect-Location", "https://example.com/redirect")
+                .WithHeader("_X-Correlation-ID", "307-temporaryredirect-guid-fss-file-download")
+                .WithBody("Temporary Redirect")
+        );
 }
