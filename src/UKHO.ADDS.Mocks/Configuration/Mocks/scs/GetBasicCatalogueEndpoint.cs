d63191b2482ed89f6f3419e1593949feef6fda1f
John Rippington <john.rippington@ukho.gov.uk>
2025-04-28 15:25:43 +0100

    Added SCS endpoint, removed old override config folders

---
+++ src/UKHO.ADDS.Mocks/Configuration/Mocks/scs/GetBasicCatalogueEndpoint.cs
@@ -0,0 +1,48 @@
+﻿using UKHO.ADDS.Mocks.Mime;
+using UKHO.ADDS.Mocks.States;
+
+namespace UKHO.ADDS.Mocks.Configuration.Mocks.scs
+{
+    public class GetBasicCatalogueEndpoint : ServiceEndpointMock
+    {
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint)
+        {
+            endpoint.MapGet("/v2/catalogues/{productType}/basic", (string productType, HttpRequest request) =>
+                {
+                    var state = GetState(request);
+
+                    switch (state)
+                    {
+                        case WellKnownState.Default:
+
+                            switch (productType.ToLowerInvariant())
+                            {
+                                case "s100":
+                                    var pathResult = endpoint.GetFile("s100-catalogue.json");
+
+                                    if (pathResult.IsSuccess(out var file))
+                                    {
+                                        return Results.File(file.Path, contentType: MimeType.Application.Json);
+                                    }
+
+                                    return Results.NotFound("Could not find the path in the /files GET method");
+                                default:
+                                    return Results.NotFound();
+                            }
+
+                        default:
+                            // Just send default responses
+                            return DefaultStateHandler.HandleDefaultState(state);
+                    }
+                })
+                .Produces<string>()
+                .WithEndpointMetadata(endpoint, d =>
+                {
+                    d.Bold("Gets a basic catalogue")
+                        .AppendNewLine()
+                        .Italic("Only s100 is implemented at the moment")
+                        .AppendNewLine();
+                });
+        }
+    }
+}
