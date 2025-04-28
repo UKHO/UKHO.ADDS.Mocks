7578c469c82ce6fc9380708588c8c04d1a30a603
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-04-28 16:08:48 +0100

    Add Fss Endpoints to - Add File - Create Batch

---
+++ src/UKHO.ADDS.Mocks/Configuration/Mocks/fss/AddFileEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using UKHO.ADDS.Mocks.States;
+
+namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
+{
+    public class AddFileEndpoint : ServiceEndpointMock
+    {
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint)
+        {
+            endpoint.MapPost("/batch/{batchId}/files/{fileName}", (HttpRequest request) =>
+            {
+                var state = GetState(request);
+
+                switch (state)
+                {
+                    case WellKnownState.Default:
+                        // ADDS Mock will have the 'default' state unless we have told it otherwise
+                        return Results.Created();
+
+                    default:
+                        // Just send default responses
+                        return DefaultStateHandler.HandleDefaultState(state);
+                }
+            })
+                .Produces<string>()
+                .WithEndpointMetadata(endpoint, d =>
+                {
+                    d.Bold("Add a file")
+                        .AppendNewLine()
+                        .Italic("Just returns a 201, won't actually create anything");
+                });
+        }
+    }
+}
