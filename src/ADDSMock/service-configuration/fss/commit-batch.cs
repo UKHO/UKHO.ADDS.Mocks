f5e6569f5fa26d98d5daf97206b7357bad5bc50e
Harshala Lavate <harshal15305@mastek.com>
2025-04-28 11:53:36 +0530

    code changes AB#215473

---
+++ src/ADDSMock/service-configuration/fss/commit-batch.cs
@@ -0,0 +1,49 @@
+﻿using ADDSMock.Domain.Mappings;
+using System.Net;
+using WireMock.Matchers;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using System.Text.RegularExpressions;
+using WireMock.Http;
+using System.Linq;
+using WireMock.Types;
+using System;
+
+public void RegisterFragment(WireMockServer server, MockService mockService)
+{
+    var urlPattern = ".*/batch/(.*)";
+
+    server
+        .Given(
+            Request.Create()
+                .WithPath(new RegexMatcher(@"/batch/[a-fA-F0-9-]{36}")) 
+                .UsingPut() 
+                .WithBody("[{\"filename\":\"string\",\"hash\":\"string\"}]") 
+        )
+        .RespondWith(
+            Response.Create()
+                .WithCallback(request =>
+                {
+                    var batchId = request.PathSegments.ElementAtOrDefault(2); 
+                    return new WireMock.ResponseMessage
+                    {
+                        StatusCode = 202, 
+                        Headers = new Dictionary<string, WireMockList<string>>
+                        {
+                            { "Content-Type", new WireMockList<string>("application/json") }
+                        },
+                        BodyData = new WireMock.Util.BodyData
+                        {
+                            BodyAsJson = new
+                            {
+                                status = new
+                                {
+                                    uri = $"/batch/{batchId}/status" 
+                                }
+                            },
+                            DetectedBodyType = BodyType.Json 
+                        }
+                    };
+                })
+        );
+}
