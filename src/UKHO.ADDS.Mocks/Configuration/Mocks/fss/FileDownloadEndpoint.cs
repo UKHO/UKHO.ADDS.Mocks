8edc37077edb305a7adc1bf6ee49a37ad323b76a
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-05-07 12:44:42 +0100

    Add FileDownloadEndpoint.

---
+++ src/UKHO.ADDS.Mocks/Configuration/Mocks/fss/FileDownloadEndpoint.cs
@@ -0,0 +1,40 @@
+﻿using UKHO.ADDS.Mocks.Markdown;
+using UKHO.ADDS.Mocks.Mime;
+using UKHO.ADDS.Mocks.States;
+
+namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
+{
+    public class FileDownloadEndpoint : ServiceEndpointMock
+    {
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
+            endpoint.MapGet("/batch/{batchId}/files/{fileName}", (string batchId, string fileName, HttpRequest request) =>
+            {
+                var state = GetState(request);
+
+                switch (state)
+                {
+                    case WellKnownState.Default:
+
+                        var pathResult = endpoint.GetFile("readme.txt");
+
+                        if (pathResult.IsSuccess(out var file))
+                        {
+                            return Results.File(file.Path, MimeType.Text.Plain);
+                        }
+
+                        return Results.NotFound("Could not find the path in the /files GET method");
+
+                    default:
+                        // Just send default responses
+                        return WellKnownStateHandler.HandleWellKnownState(state);
+                }
+            })
+                .Produces<string>()
+                .WithEndpointMetadata(endpoint, d =>
+                {
+                    d.Append(new MarkdownHeader("Download a file", 3));
+                    d.Append(new MarkdownParagraph("Downloads readme.txt."));
+                });
+    }
+
+}
