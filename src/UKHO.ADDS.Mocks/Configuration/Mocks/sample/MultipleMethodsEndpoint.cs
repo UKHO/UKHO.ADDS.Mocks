f8f6d58657754d9c869efd11c9a7328cd8bfa8f9
John Rippington <john@portolan.co>
2025-05-13 07:45:01 +0100

    Multiple HTTP method example in sample service

---
+++ src/UKHO.ADDS.Mocks/Configuration/Mocks/sample/MultipleMethodsEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using UKHO.ADDS.Mocks.Markdown;
+
+namespace UKHO.ADDS.Mocks.Configuration.Mocks.sample
+{
+    public class MultipleMethodsEndpoint : ServiceEndpointMock
+    {
+        private readonly string[] _methods;
+
+        public MultipleMethodsEndpoint()
+        {
+            _methods = new[] { HttpMethod.Get.ToString(), HttpMethod.Post.ToString(), HttpMethod.Head.ToString(), HttpMethod.Options.ToString() };
+        }
+
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint)
+        {
+            endpoint.MapMethods("/multiples", _methods, (HttpRequest request) =>
+                {
+                    var state = GetState(request);
+
+                    return Results.Ok("Something happened");
+                })
+                .WithEndpointMetadata(endpoint, d =>
+                {
+                    d.Append(new MarkdownHeader("Demonstrates multiple HTTP methods", 3));
+                    d.Append(new MarkdownParagraph("Just a demo method, nothing too exciting"));
+                });
+        }
+    }
+}
