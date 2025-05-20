8d6efdb45fc7dc53caafd3a20420d1de47588f2b
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-05-20 18:48:29 +0100

    Enhance error handling in FSS endpoint classes
    Added `HttpResponse` parameter to endpoint methods for improved response handling. Implemented new cases for well-known states (`BadRequest`, `NotFound`, `UnsupportedMediaType`, `InternalServerError`) to return detailed JSON responses with `correlationId` and relevant error details.
    
    These changes apply consistently across multiple endpoint classes, including `BatchExpiryEndpoint`, `CommitBatchEndpoint`, `CreateBatchEndpoint`, `FileDownloadEndpoint`, `GetFssBatchAttributesEndpoint`, `GetFssBatchesEndpoint`, `UploadBlockEndpoint`, and `WriteBlockEndpoint`, improving the overall robustness and clarity of API responses.

--- src/UKHO.ADDS.Mocks/Configuration/Mocks/fss/BatchExpiryEndpoint.cs
+++ src/UKHO.ADDS.Mocks/Configuration/Mocks/fss/BatchExpiryEndpoint.cs
@@ -1,4 +1,5 @@
-﻿using UKHO.ADDS.Mocks.Markdown;
+﻿using UKHO.ADDS.Mocks.Headers;
+using UKHO.ADDS.Mocks.Markdown;
 using UKHO.ADDS.Mocks.States;
 
 namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
@@ -6,8 +7,9 @@ namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
     public class BatchExpiryEndpoint: ServiceEndpointMock
     {
         public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
-            endpoint.MapPut("/batch/{batchId}/expiry", (string batchId, HttpRequest request) =>
+            endpoint.MapPut("/batch/{batchId}/expiry", (string batchId, HttpRequest request, HttpResponse response) =>
             {
+                EchoHeaders(request, response, [WellKnownHeader.CorrelationId]);
                 var state = GetState(request);
 
                 var rawRequestBody = new StreamReader(request.Body).ReadToEnd();
@@ -27,6 +29,43 @@ namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
                 {
                     case WellKnownState.Default:
                         return Results.NoContent();
+
+                    case WellKnownState.BadRequest:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            errors = new[]
+                            {
+                                    new
+                                    {
+                                        source = "Batch expiry",
+                                        description = "Batch ID does not exist or invalid date."
+                                    }
+                                }
+                        }, statusCode: 400);
+
+                    case WellKnownState.NotFound:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            details = "Not Found"
+                        }, statusCode: 404);
+
+                    case WellKnownState.UnsupportedMediaType:
+                        return Results.Json(new
+                        {
+                            type = "https://example.com",
+                            title = "Unsupported Media Type",
+                            status = 415,
+                            traceId = "00-012-0123-01"
+                        }, statusCode: 415);
+
+                    case WellKnownState.InternalServerError:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            details = "Internal Server Error"
+                        }, statusCode: 500);
 
                     default:
                         // Just send default responses
--- src/UKHO.ADDS.Mocks/Configuration/Mocks/fss/UploadBlockEndpoint.cs
+++ src/UKHO.ADDS.Mocks/Configuration/Mocks/fss/UploadBlockEndpoint.cs
@@ -1,4 +1,5 @@
-﻿using UKHO.ADDS.Mocks.Markdown;
+﻿using UKHO.ADDS.Mocks.Headers;
+using UKHO.ADDS.Mocks.Markdown;
 using UKHO.ADDS.Mocks.States;
 
 namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
@@ -6,8 +7,9 @@ namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
     public class UploadBlockEndpoint : ServiceEndpointMock
     {
         public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
-            endpoint.MapPut("/batch/{batchId}/files/{fileName}/{blockId}", (string batchId, string filename, string blockId, HttpRequest request) =>
+            endpoint.MapPut("/batch/{batchId}/files/{fileName}/{blockId}", (string batchId, string filename, string blockId, HttpRequest request, HttpResponse response) =>
             {
+                EchoHeaders(request, response, [WellKnownHeader.CorrelationId]);
                 var state = GetState(request);
 
                 var rawRequestBody = new StreamReader(request.Body).ReadToEnd();
@@ -24,7 +26,42 @@ namespace UKHO.ADDS.Mocks.Configuration.Mocks.fss
 
                             return Results.Created();
 
-                            
+                    case WellKnownState.BadRequest:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            errors = new[]
+                            {
+                                    new
+                                    {
+                                        source = "Upload Block",
+                                        description = "Invalid batchId."
+                                    }
+                                }
+                        }, statusCode: 400);
+
+                    case WellKnownState.NotFound:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            details = "Not Found"
+                        }, statusCode: 404);
+
+                    case WellKnownState.UnsupportedMediaType:
+                        return Results.Json(new
+                        {
+                            type = "https://example.com",
+                            title = "Unsupported Media Type",
+                            status = 415,
+                            traceId = "00-012-0123-01"
+                        }, statusCode: 415);
+
+                    case WellKnownState.InternalServerError:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            details = "Internal Server Error"
+                        }, statusCode: 500);
 
                     default:
                         // Just send default responses
