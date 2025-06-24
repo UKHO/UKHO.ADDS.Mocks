8c433284026f43be523ebc8dc0049f712d162bf4
rushike15306 <108336693+rushike15306@users.noreply.github.com>
2025-06-24 18:55:21 +0530

    Code Changes AB#227938
    Add product names endpoint and response JSON file
    
    - Introduced `productnamesresponse.json` for product counts and details.
    - Registered a new endpoint in `ProductNamesEndpoint.cs` to handle POST requests for "s100" product type, with appropriate state-based responses.
    - Updated project file to ensure correct inclusion of the new JSON file.

---
+++ src/UKHO.ADDS.Mocks/Configuration/Mocks/scs/ProductNamesEndpoint.cs
@@ -0,0 +1,90 @@
+﻿using UKHO.ADDS.Mocks.Headers;
+using UKHO.ADDS.Mocks.Markdown;
+using UKHO.ADDS.Mocks.States;
+
+namespace UKHO.ADDS.Mocks.Configuration.Mocks.scs
+{
+    public class ProductNamesEndpoint : ServiceEndpointMock
+    {
+        public override void RegisterSingleEndpoint(IEndpointMock endpoint) =>
+            endpoint.MapPost("/v2/products/{productType}/ProductNames", (string productType, HttpRequest request, HttpResponse response) =>
+            {
+                EchoHeaders(request, response, [WellKnownHeader.CorrelationId]);
+                var state = GetState(request);
+
+                var rawRequestBody = new StreamReader(request.Body).ReadToEnd();
+                if (string.IsNullOrEmpty(rawRequestBody))
+                {
+                    return Results.BadRequest("Body required");
+                }
+
+                switch (state)
+                {
+                    case WellKnownState.Default:
+                    {
+                        switch (productType.ToLowerInvariant())
+                        {
+                            case "s100":
+                                var pathResult = GetFile("productnamesresponse.json");
+
+                                if (pathResult.IsSuccess(out var file))
+                                {
+                                    return Results.File(file.Open(), file.MimeType);
+                                }
+
+                                return Results.NotFound("Could not find the path in the /files GET method");
+
+                            default:
+                                return Results.BadRequest("No productType set");
+                        }
+                    }
+                    case WellKnownState.BadRequest:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            errors = new[]
+                            {
+                                    new
+                                    {
+                                        source = "Product Names",
+                                        description = "Bad Request."
+                                    }
+                                }
+                        }, statusCode: 400);
+
+                    case WellKnownState.NotFound:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            details = "Not Found"
+                        }, statusCode: 404);
+
+                    case WellKnownState.UnsupportedMediaType:
+                        return Results.Json(new
+                        {
+                            type = "https://example.com",
+                            title = "Unsupported Media Type",
+                            status = 415,
+                            traceId = "00-012-0123-01"
+                        }, statusCode: 415);
+
+                    case WellKnownState.InternalServerError:
+                        return Results.Json(new
+                        {
+                            correlationId = request.Headers[WellKnownHeader.CorrelationId],
+                            details = "Internal Server Error"
+                        }, statusCode: 500);
+
+                    default:
+                        // Just send default responses
+                        return WellKnownStateHandler.HandleWellKnownState(state);
+                }
+            })
+                .Produces<string>()
+                .WithEndpointMetadata(endpoint, d =>
+                {
+                    d.Append(new MarkdownHeader("Get Product Names", 3));
+                    d.Append(new MarkdownParagraph("Returns a list of product names for the specified product type. The request body must not be empty. Only 's100' is currently supported."));
+                });
+    }
+}
