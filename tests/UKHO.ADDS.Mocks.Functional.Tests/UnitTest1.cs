bd4eef3c88ac98e9aaa61d162fa683cedeeee8e7
hukho <128484816+hukho@users.noreply.github.com>
2026-04-09 10:06:57 +0100

    Update ft project

---
+++ tests/UKHO.ADDS.Mocks.Functional.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace UKHO.ADDS.Mocks.Functional.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
